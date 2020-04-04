ad18bd83e31c027e4e2e68a566022045f5ef03b5
stscoundrel <silvennoinen.sampo@gmail.com>
2020-04-04 17:55:05 +0300

    Examples: add CLI migration example

---
+++ examples/MovieMigration.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Your\Namespace;
+
+// Ouroboros classes.
+use Silvanus\Ouroboros\Migration;
+use Silvanus\Ouroboros\Schema;
+
+/**
+ * MovieMigration class to be called in CLI.
+ */
+class MovieMigration extends Migration {
+
+	/**
+	 * CLI command base.
+	 * Use "wp ouroboros migrate movies up" or down.
+	 */
+	protected $command = 'movies';
+
+	/**
+	 * Set Schema object to props.
+	 * Only required method.
+	 */
+	public function set_schema() {
+
+		$columns = array(
+			'id' => 'bigint(20) NOT NULL AUTO_INCREMENT',
+			'name' => 'varchar(255) NOT NULL',
+		);
+
+		$this->schema = new Schema( 'movies', $columns );
+	}
+}
