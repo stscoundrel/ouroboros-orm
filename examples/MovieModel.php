f375843635cf2a95688e75e605a3a3f2c54e9fac
stscoundrel <silvennoinen.sampo@gmail.com>
2020-04-04 19:26:29 +0300

    Examples: add model example

---
+++ examples/MovieModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Your\Namespace;
+
+// Ouroboros classes.
+use Silvanus\Ouroboros\Model;
+
+class Movie extends Model {
+
+	/**
+	 * Name of database without prefix,
+	 */
+	protected static $table = 'movies';
+
+	/**
+	 * Optional: primary key.
+	 * Defaults to "id"
+	 */
+	protected static $primary_key = 'id';
+
+
+	/**
+	 * Thats it!
+	 * You can add your custom model method here.
+	 * Or use standard ones like:
+	 * - Find
+	 * - Create
+	 * - Update
+	 * - Delete
+	 */
+
+}
