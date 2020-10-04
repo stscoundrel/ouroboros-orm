b6ec7ad10b43276cdae2c7213db4ef4fc979f791
stscoundrel <sampo@pixels.fi>
2020-10-04 13:26:47 +0300

    Tests: add Migration tests for setting schemas & hooking command

---
+++ tests/TestDoubles/WP_CLI.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Mock WP_CLI class.
+ */
+class WP_CLI
+{
+
+    /**
+     * Store calls to add commands.
+     */
+    public static $added_commands = array();
+
+    /**
+     * Store success messages.
+     */
+    public static $successes = array();
+
+    public static function add_command(string $command, $class ) {
+        static::$added_commands[] = array( $command, $class );
+    }
+
+    public static function success(string $message ) {
+        static::$successes[] = $message;
+    }
+}
